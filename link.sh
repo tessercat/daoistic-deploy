eb6861368b32de02d1de216f68ee2bdb2fc4475c
Peter Christensen <peter@daoistic.ca>
2019-10-18 11:08:25 -0600

    Fix role name.

--- link.sh
+++ link.sh
@@ -1,4 +1,4 @@
 PARENT=$(dirname $PWD)
 ln -s -f $PWD/daoistic-backup.yml $PARENT/stack-config/daoistic-backup.yml
 ln -s -f $PWD/daoistic-deploy.yml $PARENT/stack-config/daoistic-deploy.yml
-ln -s -f $PWD/role $PARENT/stack-config/roles/daoistic
+ln -s -f $PWD/role $PARENT/stack-config/roles/daoistic-web
