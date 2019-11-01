1050efb4ebc0521381046f0f1a211800d6b46b7e
Peter Christensen <peter@daoistic.ca>
2019-11-01 12:56:25 -0600

    Nope on exit.

--- init.sh
+++ init.sh
@@ -1,8 +1,7 @@
-# Link playbooks and roles to ../puptel/.
-
 # Link playbooks.
 PB_DIR=$(readlink -f $PWD/../playbooks)
 if [ ! -d $PB_DIR ]; then
+    echo Nope
     exit 1
 fi
 ln -sf $PWD/daoistic-backup.yml $PB_DIR/daoistic-backup.yml
